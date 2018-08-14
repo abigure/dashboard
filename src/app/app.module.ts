d0253b3462b0a9e5b1e55dd7705d59df50cb8b93
= <=>
2018-08-14 18:03:13 +0800

    page sliced

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavsideComponent } from './navi/navside.component';
+import { NavtopComponent } from './navi/navtop.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SummaryComponent } from './pages/dashboard/panels/summary.component';
+import { AreachartComponent } from './pages/dashboard/panels/areachart.component';
+import { BarchartComponent } from './pages/dashboard/panels/barchart.component';
+import { TimelineComponent } from './pages/dashboard/panels/timeline.component';
+import { NotifyComponent } from './pages/dashboard/panels/notify.component';
+import { DonutComponent } from './pages/dashboard/panels/donut.component';
+import { ChatComponent } from './pages/dashboard/panels/chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavsideComponent,
+    NavtopComponent,
+    DashboardComponent,
+    SummaryComponent,
+    AreachartComponent,
+    BarchartComponent,
+    TimelineComponent,
+    NotifyComponent,
+    DonutComponent,
+    ChatComponent
   ],
   imports: [
     BrowserModule
